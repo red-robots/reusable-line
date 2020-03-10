d99403e6f9b883671d580f882c1edb97fbf4a5a3
hermie <hermiebarit@gmail.com>
2020-03-10 16:14:06 +0800

    Reusable line/block

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Block</title>
+</head>
+<body>
+    <h3>Reusable Line</h3>
+    <?php include('reusable-line.php'); ?>
+    <p>Usage: include the file "reusable-line.php" and it will create a full width line.</p>
+    <div>
+        &lt;?php include('reusable-line.php');  ?&gt;
+    </div>
+</body>
+</html>
---
+++ reusable-line.php
@@ -0,0 +1,10 @@
+<style>
+    .custom_divider{
+        width: 100%;
+        padding-top: 15px;
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 15px;
+        display: block;
+    }
+</style>
+<div class="custom_divider"></div>
